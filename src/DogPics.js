a09fa370631cb2a15646bc95b29246916b882489
Jason Baik <baikjs87@gmail.com>
2022-06-14 03:25:17 -0400

    Completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
+function DogPics({count}) {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
@@ -13,14 +13,14 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  },[count]);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="a dog"/>
       ))}
     </div>
   );
